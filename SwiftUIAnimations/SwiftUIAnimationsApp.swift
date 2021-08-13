478be120f245b23cf40372121183c5aa05f9c190
Logan Koshenka <koshenka27@gmail.com>
2021-08-13 06:44:58 -0400

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Logan Koshenka on 8/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
